6d9afa089d4a4eb455439fcc6ab0997f8745b0b5
Balaji Narayanaswamy <balajin@tejastech.net>
2016-02-23 13:19:39 -0600

    Initial Checkin

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app';
+
+ReactDOM.render(
+	// 1) let's render a component here, which we will create shortly
+	<App />
+	// 2) Decide where in the DOM we are going to render this new component
+	,document.querySelector('.container')
+);
